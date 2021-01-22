543298db20adb4b0375f24540c4389a271aa82d2
Michael Pingel <mdpingel@gmail.com>
2021-01-22 15:00:17 -0800

    pass test 9, beepBoop() will return a string

--- js/scripts.js
+++ js/scripts.js
@@ -184,6 +184,40 @@
 // Test: "It should return an array starting and ending with ["Beep!", "Boop!", "Won't you be my neighbor?", 4, 5, ... "Boop!", "Won't you be my neighbor?", "Won't you be my neighbor?", "Won't you be my neighbor?", "Won't you be my neighbor?"] if the number 33 is inputted"
 // Expect(beepBoop(33).toEqual(["Beep!", "Boop!", "Won't you be my neighbor?", 4, 5, ... "Boop!", "Won't you be my neighbor?", "Won't you be my neighbor?", "Won't you be my neighbor?", "Won't you be my neighbor?"]);
 
+// function splitDigits(number) {
+//   let digitArray = [];
+//   let digitString = number.toString();
+//   for (let digitIndex = 0, numberLength = digitString.length; digitIndex < numberLength; digitIndex ++) {
+//     digitArray.push(+digitString.charAt(digitIndex));
+//   }
+//   return digitArray
+// };
+
+// function beepBoop(number) {
+//   let array2 = [];
+//   for (let index = 1; index <= number; index ++) {
+//     if (index > 3 && index < 10) {
+//       array2.push(index);
+//     } else {
+//       const digitsArray = splitDigits(index);
+//       if (digitsArray.includes(3)) {
+//         array2.push("Won't you be my neighbor?");
+//       } else if (digitsArray.includes(2)) {
+//         array2.push("Boop!");
+//       } else if (digitsArray.includes(1)) {
+//         array2.push("Beep!");
+//       } else {
+//         array2.push(index);
+//       }
+//     }
+//   };
+//   return array2;
+// };
+// console.log(beepBoop(33));
+
+// Test: "It should return a string with "Beep!, Boop!, Won't you be my neighbor?, 4, 5, 6, 7, 8, 9, Beep!" if the number 10 is inputted"
+// Expect(beepBoop(10).toEqual("Beep!, Boop!, Won't you be my neighbor?, 4, 5, 6, 7, 8, 9, Beep!");
+
 function splitDigits(number) {
   let digitArray = [];
   let digitString = number.toString();
@@ -211,6 +245,7 @@ function beepBoop(number) {
       }
     }
   };
-  return array2;
+  robogersString = array2.join(" ");
+  return robogersString;
 };
-console.log(beepBoop(555));
+console.log("Mr. Robogers says " + beepBoop(13));
