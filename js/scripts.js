0199baefb481ce069ca30ca2ea47b7caeb35c87d
Michael Pingel <mdpingel@gmail.com>
2021-01-22 14:54:14 -0800

    pass test 8, beepBoop(33) will return desired results, remove redundant spec from readme

--- js/scripts.js
+++ js/scripts.js
@@ -151,6 +151,39 @@
 // Test: "It should return an array starting and ending with ["Beep!", "Boop!", 3, 4, ... "Beep!", "Boop!", "Boop!", "Boop!"] if the number 22 is inputted"
 // Expect(beepBoop(22).toEqual(["Beep!", "Boop!", 3, 4, ... "Beep!", "Boop!", "Boop!", "Boop!"]);
 
+// function splitDigits(number) {
+//   let digitArray = [];
+//   let digitString = number.toString();
+//   for (let digitIndex = 0, numberLength = digitString.length; digitIndex < numberLength; digitIndex ++) {
+//     digitArray.push(+digitString.charAt(digitIndex));
+//   }
+//   return digitArray
+// };
+
+// function beepBoop(number) {
+//   array2 = [];
+//   for (let index = 1; index <= number; index ++) {
+//     if (index > 2 && index < 10) {
+//       array2.push(index);
+//     } else {
+//       const digitsArray = splitDigits(index);
+//       if (digitsArray.includes(2)) {
+//         array2.push("Boop!");
+//       } else if (digitsArray.includes(1)) {
+//         array2.push("Beep!");
+//       } else {
+//         array2.push(index);
+//       }
+//     }
+//   };
+//   return array2;
+// };
+
+// console.log(beepBoop(22));
+
+// Test: "It should return an array starting and ending with ["Beep!", "Boop!", "Won't you be my neighbor?", 4, 5, ... "Boop!", "Won't you be my neighbor?", "Won't you be my neighbor?", "Won't you be my neighbor?", "Won't you be my neighbor?"] if the number 33 is inputted"
+// Expect(beepBoop(33).toEqual(["Beep!", "Boop!", "Won't you be my neighbor?", 4, 5, ... "Boop!", "Won't you be my neighbor?", "Won't you be my neighbor?", "Won't you be my neighbor?", "Won't you be my neighbor?"]);
+
 function splitDigits(number) {
   let digitArray = [];
   let digitString = number.toString();
@@ -161,13 +194,15 @@ function splitDigits(number) {
 };
 
 function beepBoop(number) {
-  array2 = [];
+  let array2 = [];
   for (let index = 1; index <= number; index ++) {
-    if (index > 2 && index < 10) {
+    if (index > 3 && index < 10) {
       array2.push(index);
     } else {
       const digitsArray = splitDigits(index);
-      if (digitsArray.includes(2)) {
+      if (digitsArray.includes(3)) {
+        array2.push("Won't you be my neighbor?");
+      } else if (digitsArray.includes(2)) {
         array2.push("Boop!");
       } else if (digitsArray.includes(1)) {
         array2.push("Beep!");
@@ -178,5 +213,4 @@ function beepBoop(number) {
   };
   return array2;
 };
-
-console.log(beepBoop(22));
+console.log(beepBoop(555));
