f4a549a17a83090dadbc304f6158b031309ea717
Michael Pingel <mdpingel@gmail.com>
2021-01-22 10:05:09 -0800

    Test 2 passed, beepBoop() returns '0' when 0 is entered

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,19 @@
 // Test: "It should return an array with a 0 if the number 0 is inputted"
 // Expect(beepBoop(0).toEqual([0]);
 
+// function beepBoop(number) {
+//   const robogersArray = [number];
+//   return robogersArray
+// };
+
+// console.log(beepBoop(0));
+
+// Test: "It should return a string with "0" if the number 0 is inputted"
+// Expect(beepBoop(0).toEqual("0");
+
 function beepBoop(number) {
   const robogersArray = [number];
-  return robogersArray
+  return robogersArray.toString();
 };
 
-console.log(beepBoop(0));
+console.log("Mr. Robergers says: " + beepBoop(0));
