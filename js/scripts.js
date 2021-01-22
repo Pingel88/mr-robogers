3ecbeb63d3c631baceb812eba3ea26e6963d8291
Michael Pingel <mdpingel@gmail.com>
2021-01-22 12:48:55 -0800

    add javascript functionality to split a number of an infinite length into individual digits

--- js/scripts.js
+++ js/scripts.js
@@ -69,18 +69,45 @@
 // console.log(beepBoop(10));
 
 // Test: "It should return an array with [Beep!, 2, 3, 4, 5, 6, 7, 8, 9] if the number 9 is inputted"
-// Expect(beepBoop(10).toEqual([Beep!, 2, 3, 4, 5, 6, 7, 8, 9]);
+// Expect(beepBoop(9).toEqual([Beep!, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+// function beepBoop(number) {
+//   let robogersArray = [];
+//   for (let index = 1; index <= number; index ++) {
+//     if (index === 1) {
+//       robogersArray.push("Beep!");
+//     } else {
+//       robogersArray.push(index);
+//     }
+//   } 
+//   return robogersArray
+// };
+
+// console.log(beepBoop(9));
+
+// Test: "It should return an array with [Beep!, Boop!, 3, 4, 5, 6, 7, 8, 9, "Beep!"] if the number 10 is inputted"
+// Expect(beepBoop(10).toEqual([Beep!, Boop!, 3, 4, 5, 6, 7, 8, 9, "Beep!"]);
+
+function splitDigits(number) {
+  let digitArray = [];
+  let digitString = number.toString();
+  for (let digitIndex = 0, numberLength = digitString.length; digitIndex < numberLength; digitIndex ++) {
+    digitArray.push(+digitString.charAt(digitIndex));
+  }
+  return digitArray
+};
 
 function beepBoop(number) {
   let robogersArray = [];
   for (let index = 1; index <= number; index ++) {
-    if (index === 1) {
-      robogersArray.push("Beep!");
-    } else {
-      robogersArray.push(index);
-    }
-  } 
-  return robogersArray
+  const digitArray = splitDigits(index);
+  }
+  // if (index === 1) {
+  //   robogersArray.push("Beep!");
+  // } else {
+  //   robogersArray.push(index);
+  // }
+  // return robogersArray
 };
 
-console.log(beepBoop(10));
+console.log(beepBoop(123));
